322cd5a2f712f7cbf3cc2df14fb165eb1e0a89e9
digeler <dinorg@gmail.com>
2020-01-29 12:47:59 +0200

    fixed runtime

--- Class1.cs
+++ Class1.cs
@@ -6,9 +6,10 @@ namespace clrmd
 {
     public class Clrmdhelper
     {
-       private string path;
-       private  string mscrdwaksloc;
+        private string path;
+        private string mscrdwaksloc;
         private string symbolserverEnv;
+        private ClrRuntime runtime;
 
         public Clrmdhelper()
         {
@@ -24,6 +25,22 @@ namespace clrmd
             string sym = "https://msdl.microsoft.com/download/symbols";
             Console.WriteLine("using default env sympbol {0}", sym);
             Environment.SetEnvironmentVariable("_NT_SYMBOL_PATH", sym);
+            Console.WriteLine("Enter Dump Path");
+            Path = Console.ReadLine();
+           
+            Console.WriteLine("Enter Dac Location,If Unknown Leave Blank");
+            Mscrdwaksloc = Console.ReadLine();
+            Console.WriteLine("Enter Symbol Server Location ,default is public");
+            SymbolserverEnv = Console.ReadLine();
+            if (SymbolserverEnv == null)
+            {
+                SymbolserverEnv = sym;  
+            }
+            Setenv();
+            Dumpinfo();
+            ClrRuntime runtime = Runtime();
+
+
 
         }
 
@@ -33,6 +50,8 @@ namespace clrmd
             set { path = value; }
         }
 
+        
+
         public string Mscrdwaksloc
         {
             get { return mscrdwaksloc; }
@@ -40,18 +59,20 @@ namespace clrmd
 
         }
 
-        public string SymbolserverEnv{
+        public string SymbolserverEnv
+        {
 
             get { return symbolserverEnv; }
             set { symbolserverEnv = value; }
 
-}
+        }
 
-        public void Loadump()
+        public void Dumpinfo()
         {
             using (DataTarget dataTarget = DataTarget.LoadCrashDump(path))
             {
-                if (File.Exists(path)){
+                if (File.Exists(path))
+                {
 
                     Console.WriteLine("Loaded Dumpfile name {0}", path);
                 }
@@ -71,46 +92,73 @@ namespace clrmd
                     Console.WriteLine("Dac File:  {0}", dacInfo.FileName);
                     ClrInfo runtimeInfo = dataTarget.ClrVersions[0];
 
+
                     // If we just happen to have the correct dac file installed on the machine,
                     // the "LocalMatchingDac" property will return its location on disk:
                     string dacLocation = version.LocalMatchingDac;
                     if (!string.IsNullOrEmpty(dacLocation))
-                    
-                    {
                         Console.WriteLine("Local dac location: " + dacLocation);
-                    }
-                    else
-                    {
-                        if (File.Exists(mscrdwaksloc))
-                        {
-                            Console.WriteLine("using {0} for dac loc", mscrdwaksloc);
-                            _ = runtimeInfo.CreateRuntime(@dacLocation);
 
-                        }
-
-                        else
-                        {
-                            Console.WriteLine("downloading dac from sympbol server");
-                            ClrRuntime runtime = runtimeInfo.CreateRuntime();
-                        }
-
-                    }
-                   
-
-                    
                 }
 
+
+
+
             }
+
         }
+
+
 
         public void Setenv()
 
         {
             Environment.SetEnvironmentVariable("_NT_SYMBOL_PATH", symbolserverEnv);
-           var enval = Environment.GetEnvironmentVariable("_NT_SYMBOL_PATH");
+            var enval = Environment.GetEnvironmentVariable("_NT_SYMBOL_PATH");
             Console.WriteLine("Symbol server is set to {0}", enval);
+
+
 
         }
 
+        public ClrRuntime Runtime()
+        {
+
+            if (File.Exists(mscrdwaksloc))
+            {
+                Console.WriteLine("using {0} for dac loc", mscrdwaksloc);
+                DataTarget dataTarget = DataTarget.LoadCrashDump(path);
+                ClrInfo runtimeInfo = dataTarget.ClrVersions[0];
+                ClrRuntime runtime = runtimeInfo.CreateRuntime(mscrdwaksloc);
+                return runtime;
+
+            }
+
+
+
+          if (!File.Exists(mscrdwaksloc))
+            {
+                DataTarget dataTarget = DataTarget.LoadCrashDump(path);
+                ClrInfo runtimeInfo = dataTarget.ClrVersions[0];
+                ClrRuntime runtime = runtimeInfo.CreateRuntime();
+                return runtime;
+            }
+
+
+            return null;
+        }
+
+
+        public void Getappdomains()
+        {
+            foreach (ClrAppDomain domain in runtime.AppDomains)
+            {
+                Console.WriteLine("ID:      {0}", domain.Id);
+                Console.WriteLine("Name:    {0}", domain.Name);
+                Console.WriteLine("Address: {0}", domain.Address);
+            }
+        }
+
+        }
     }
-}
+
